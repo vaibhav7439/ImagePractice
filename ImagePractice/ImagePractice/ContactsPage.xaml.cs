b0614494e69094e8702839b38e9e5aa11077e175
vaibhav7439 <singhvaibhav022@gmail.com>
2021-09-24 12:41:15 +0530

    Add project files.

---
+++ ImagePractice/ImagePractice/ContactsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ImagePractice
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContactsPage : ContentPage
+    {
+        public ContactsPage()
+        {
+            InitializeComponent();
+            myList.ItemsSource = new List<String>
+            {
+                "Saurabh",
+                "Vaibhav",
+                "Shilpi"
+            };
+        }
+    }
+}
---
+++ ImagePractice/ImagePractice/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImagePractice
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            imageView.Source = "https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW5kaWF8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80";
+        }
+    }
+}
